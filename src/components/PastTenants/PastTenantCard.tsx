2e1fda88babe47584fe7ccb25d1c5eb97fb69f33
Eashley Martínez <113074720+eashpie7u7@users.noreply.github.com>
2025-04-12 19:31:20 -0600

    added past tenants list view

---
+++ src/components/PastTenants/PastTenantCard.tsx
@@ -0,0 +1,57 @@
+// components/PastTenantCard.tsx
+import { FC } from "react";
+import Image from "next/image";
+export type FullStarRating = 0 | 1 | 2 | 3 | 4 | 5;
+import profile_pic from "./profile_pic.jpg";
+import { IoMdStarOutline } from "react-icons/io";
+
+import StarRating from "../StarRating/StarRating";
+export interface PastTenant {
+  id: string;
+  name: string;
+  specialty: string;
+  agreementDate: string;
+  rating: FullStarRating;
+}
+
+interface Props {
+  tenant: PastTenant;
+}
+
+const PastTenantCard: FC<Props> = ({ tenant }) => {
+  return (
+    <div className="border rounded-lg shadow-sm p-4 bg-white flex items-center justify-between">
+      <div className="flex items-center gap-3">
+        <Image
+          src={profile_pic}
+          alt={tenant.name}
+          className="w-14 h-14 rounded-full object-cover"
+        />
+        <div className="flex flex-col">
+          <div className="flex flex-row">
+            <h3 className="text-base font-semibold">{tenant.name}</h3>
+
+            <div className="flex items-center space-x-1">
+              <IoMdStarOutline className="text-blue-500 ml-4" />
+              <p className="text-base font-semibold">{tenant.rating}</p>
+            </div>
+          </div>
+
+          <p className="text-sm text-gray-500">
+            {tenant.specialty} <span className="mx-1">|</span>{" "}
+            {tenant.agreementDate}
+          </p>
+        </div>
+      </div>
+
+      <div className="flex items-center gap-3">
+        <StarRating />
+        <button className="bg-blue-500 text-white text-sm px-4 py-1.5 rounded-md">
+          Rate
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default PastTenantCard;
