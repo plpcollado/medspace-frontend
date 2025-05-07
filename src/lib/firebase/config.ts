e4b66158e1726116b798a0ec7c662dd74b17c7b7
JoseAE <107892696+Jose-AE@users.noreply.github.com>
2025-05-06 23:38:17 -0600

    fix(firebase): refactor Firebase config to use environment variables

--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -1,8 +1,10 @@
+import { env } from "@/config/env";
+
 export const firebaseConfig = {
-  apiKey: "AIzaSyCG2WxjPOzM94W2UzFWmDnJv29HzcpiA_A",
-  authDomain: "med-space-c2eba.firebaseapp.com",
-  projectId: "med-space-c2eba",
-  storageBucket: "med-space-c2eba.firebasestorage.app",
-  messagingSenderId: "567161984682",
-  appId: "1:567161984682:web:35bf048cdbed4536b5a2d3"
+  apiKey: env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
