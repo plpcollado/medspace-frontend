282dd98d673f58111e1e875443e340d150535bc7
Eashley Martínez <113074720+eashpie7u7@users.noreply.github.com>
2025-05-11 16:21:28 -0600

    Added zip code validation

--- src/app/main/landlord/create-clinic/steps/BasicInfoSection.tsx
+++ src/app/main/landlord/create-clinic/steps/BasicInfoSection.tsx
@@ -4,7 +4,11 @@ import TextInput from "@/components/TextInput";
 import SelectInput from "@/components/SelectInput/SelectInput";
 import ClinicEquipmentTag from "@/components/ClinicEquipmentTag";
 import { constToTitleCase } from "@/lib/textUtils";
+import MapInput from "../steps/../../../../../components/MapInput/MapInput";
 import toast from "react-hot-toast";
+import { useMapAddress } from "@/hooks/useMapAddress";
+import { Coordinates } from "@/hooks/useMapAddress";
+import { useEffect } from "react";
 
 export default function BasicInfoSection({
   onClickPrimary,
@@ -19,6 +23,16 @@ export default function BasicInfoSection({
     value: cat,
     name: constToTitleCase(cat)
   }));
+  const INITIAL_COORDS = {
+    latitude: 19.4326,
+    longitude: -99.1332
+  };
+  useEffect(() => {
+    setData("addressLatitude", INITIAL_COORDS.latitude);
+    setData("addressLongitude", INITIAL_COORDS.longitude);
+
+    setCoordsOnly(INITIAL_COORDS);
+  }, []);
 
   const DEFAULT_EQUIPMENT = "DEFAULT_SELECT";
   const equipmentsOptions = [
@@ -28,6 +42,30 @@ export default function BasicInfoSection({
       name: constToTitleCase(eq)
     }))
   ];
+  const {
+    coordinates,
+    address,
+    addressCity,
+    addressState,
+    addressZip,
+    addressCountry,
+    updateLocation: setCoordsOnly
+  } = useMapAddress(INITIAL_COORDS);
+  useEffect(() => {
+    if (address && address !== "Dirección no encontrada") {
+      setData("addressStreet", address);
+      setData("addressCity", addressCity);
+      setData("addressState", addressState);
+      setData("addressZip", addressZip);
+      setData("addressCountry", addressCountry);
+    }
+  }, [address]);
+
+  const updateLocation = (coords: Coordinates) => {
+    setData("addressLatitude", coords.latitude);
+    setData("addressLongitude", coords.longitude);
+    setCoordsOnly(coords);
+  };
 
   const handleAddEquipment = (eq: string) => {
     if (eq != DEFAULT_EQUIPMENT && !data.equipments?.includes(eq)) {
@@ -55,6 +93,19 @@ export default function BasicInfoSection({
       setError("size", "Size must be greater than 0");
       isValid = false;
     }
+
+    if (!data.addressStreet?.trim()) {
+      setError("addressStreet", "Address could not be determined");
+      isValid = false;
+    }
+    if (!data.addressZip?.trim()) {
+      setError("addressZip", "ZIP Code cannot be empty");
+      isValid = false;
+    } else if (!/^\d{5}$/.test(data.addressZip)) {
+      setError("addressZip", "ZIP Code must be exactly 5 digits");
+      isValid = false;
+    }
+    
     return isValid;
   };
 
@@ -110,7 +161,39 @@ export default function BasicInfoSection({
               isInvalid={!!errors.description}
             />
           </div>
-          <div className="flex-1">Map here</div>
+          <div className="flex-1 flex flex-col gap-4">
+            <div className="w-full h-[250px]">
+              <MapInput
+                onLocationChange={updateLocation}
+                className="w-full h-full rounded-lg "
+              />
+            </div>
+            <div className="flex gap-4">
+            <TextInput
+                  label="Address"
+                  value={data.addressStreet ?? ""}
+                  onChange={(e) => {
+                    clearError("addressStreet");
+                    setData("addressStreet", e.target.value);
+                  }}
+                  invalidMessage={errors.addressStreet}
+                  isInvalid={!!errors.addressStreet}
+                />
+              <div className="w-1/3">
+              <TextInput
+                label="ZIP Code"
+                value={data.addressZip ?? ""}
+                onChange={(e) => {
+                  clearError("addressZip");
+                  setData("addressZip", e.target.value);
+                }}
+                invalidMessage={errors.addressZip}
+                isInvalid={!!errors.addressZip}
+              />
+                
+              </div>
+            </div>
+          </div>
         </div>
         <div className="flex gap-12">
           <div className="flex-1">
